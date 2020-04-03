bf74574ca02b29fb843d92c5c24fca6ade3852a1
Liam Tengelis <ltengelis@gmail.com>
2020-04-03 22:10:02 +0700

    handle s3 PUT as well

--- gateway/lua/obj.lua
+++ gateway/lua/obj.lua
@@ -1,6 +1,7 @@
 local cjson = require "cjson"
 
 local aws = require "lua/aws"
+local downstream = require "lua/downstream"
 local log = require "lua/log"
 
 
@@ -19,7 +20,20 @@ local function get_obj(obj_id)
    ngx.say(res.body)
 end
 
-         -- body = downstream.get_body_string()
+local function put_obj(obj_id)
+   local res, err_code = aws.req_aws(
+      obj_id, "PUT", downstream.get_body_string()
+   )
+   if err_code then
+      ngx.status = err_code
+      ngx.say(cjson.encode({
+                    err = "obj storage failure"
+      }))
+      ngx.exit(err_code)
+      return
+   end
+   -- proceed to insert a record for this obj
+end
 
 -- public
 local function access_obj(obj_id)
@@ -27,6 +41,10 @@ local function access_obj(obj_id)
    if method == "GET" then
       return get_obj(obj_id)
    end
+   if method == "PUT" then
+      return put_obj(obj_id)
+   end
+   ngx.exit(405)
 end
 
 
