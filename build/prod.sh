fe55b0c6d5ee3331bf88f4d01fe4996f5fc42e31
Liam Tengelis <ltengelis@gmail.com>
2020-04-22 13:37:37 +0700

    add prod build script

--- build/prod.sh
+++ build/prod.sh
@@ -1,4 +1,26 @@
+#!/bin/sh
+
 dev_dir=$(dirname "$(readlink -f "$0")")
 base_dir=${dev_dir}/..
 
-# TAG=$(date -u +%Y-%m-%d:%R)
+git_changes=$(git status -s)
+if [ "${git_changes}" != "" ]; then
+    echo "working changes, won't proceed"
+    exit 1
+fi
+
+DATE_TAG=$(date -u +%Y-%m-%d-%H%M)
+GIT_TAG=$(git rev-parse --short --verify HEAD)
+TAG=${DATE_TAG}--${GIT_TAG}
+
+do_build() {
+    image_name="${1}"
+    build_dir="${2}"
+    full_image_name=galleri/${image_name}:${TAG}
+    docker build -t ${full_image_name} ${base_dir}/${build_dir} \
+        && docker push ${full_image_name}
+}
+
+do_build gateway gateway
+do_build mg2 mg2
+do_build mg2_handler mg2_handler
