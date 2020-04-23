5ac33a76f0a49be1c822dfa21377e095f8caead7
Liam Tengelis <ltengelis@gmail.com>
2020-04-23 13:19:24 +0700

    adapt API to images as array

--- db/schema/api/functions/image/api.init_collection.sql
+++ db/schema/api/functions/image/api.init_collection.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION api.init_collection(
-  IN p_obj_id                  TEXT,
+  IN p_collection_id           TEXT,
   IN p_collection_name         TEXT,
   IN p_image_ids               TEXT[]
 )
@@ -7,33 +7,25 @@ RETURNS JSON
 AS $$
 DECLARE
   v_user_id            UUID := session_user_id();
-  v_obj_id             UUID := str_to_uuid(p_obj_id);
-  v_image_id_s         TEXT;
-  v_image_id           UUID;
+  v_collection_id      UUID := str_to_uuid(p_collection_id);
+  v_image_ids          UUID[];
 BEGIN
-  -- init collection
-  PERFORM sys.image_collection_create(
-    p_user_id         => v_user_id,
-    p_obj_id          => v_obj_id,
-    p_collection_name => p_collection_name
-  );
-  -- add images
-  FOREACH v_image_id_s IN ARRAY p_image_ids LOOP
-    v_image_id := str_to_uuid(v_image_id_s);
-    IF check_owns(
-      p_user_id         => v_user_id,
-      p_obj_id          => v_image_id
-    ) THEN
-      PERFORM sys.image_collection_image_create(
-        p_image_id         => v_image_id,
-        p_collection_id    => v_obj_id
-      );
-    ELSE
+  FOR i IN array_lower(p_image_ids, 1)..array_upper(p_image_ids, 1) LOOP
+    v_image_ids[i] := str_to_uuid(p_image_ids[i]);
+    IF NOT check_owns(v_user_id, v_image_ids[i]) THEN
       RAISE insufficient_privilege;
     END IF;
   END LOOP;
+
+  PERFORM sys.image_collection_create(
+    p_user_id         => v_user_id,
+    p_collection_id   => v_collection_id,
+    p_collection_name => p_collection_name,
+    p_images          => v_image_ids
+  );
+
   RETURN json_build_object(
-    'collection_id', p_obj_id
+    'collection_id', p_collection_id
   );
 END;
 $$
