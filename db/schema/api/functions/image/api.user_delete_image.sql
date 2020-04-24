61fd04046623e499192155c270d0da20d7c9781c
Liam Tengelis <ltengelis@gmail.com>
2020-04-24 16:25:32 +0700

    image delete functions

--- db/schema/api/functions/image/api.user_delete_image.sql
+++ db/schema/api/functions/image/api.user_delete_image.sql
@@ -1,26 +1,43 @@
 CREATE OR REPLACE FUNCTION api.user_image_delete(
-  IN p_obj_id           TEXT
+  IN p_collection_id           TEXT,
+  IN p_image_id                TEXT
 )
 RETURNS JSON
 AS $$
 DECLARE
-  v_obj_id              UUID     := str_to_uuid(p_obj_id);
+  v_collection_id       UUID     := str_to_uuid(p_collection_id);
+  v_image_id            UUID     := str_to_uuid(p_image_id);
   v_user_id             UUID     := session_user_id();
   v_deleted             INTEGER  := 0;
+  v_images              UUID[];
 BEGIN
-  WITH deleted AS (
-    DELETE FROM image
-          WHERE image_id = v_obj_id
-            AND user_id = v_user_id
-      RETURNING image_id
-  )
-  SELECT count(*)
-    INTO v_deleted
-    FROM deleted
+  IF NOT check_owns(
+    p_user_id           => v_user_id,
+    p_obj_id            => v_collection_id
+  ) THEN
+    RAISE insufficient_privilege;
+  END IF;
+  IF NOT check_owns(
+  p_user_id           => v_user_id,
+  p_obj_id            => v_image_id
+  ) THEN
+  RAISE insufficient_privilege;
+  END IF;
+
+  SELECT images
+    INTO v_images
+    FROM image_collection
+   WHERE collection_id = v_collection_id
   ;
-  RETURN json_build_object(
-    'deleted', v_deleted
-  );
+  v_images := array_remove(v_images, v_image_id);
+  UPDATE image_collection
+     SET images = v_images
+   WHERE collection_id = v_collection_id
+  ;
+
+  RETURN api.user_orphan_image_delete(
+           p_image_id   => p_image_id
+         );
 END;
 $$
 LANGUAGE plpgsql
---
+++ db/schema/api/functions/image/api.user_orphan_image_delete.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE FUNCTION api.user_orphan_image_delete(
+  IN p_image_id                TEXT
+)
+RETURNS JSON
+AS $$
+DECLARE
+  v_image_id            UUID     := str_to_uuid(p_image_id);
+  v_user_id             UUID     := session_user_id();
+  v_deleted             INTEGER  := 0;
+  v_images              UUID[];
+BEGIN
+  IF NOT check_owns(
+    p_user_id           => v_user_id,
+    p_obj_id            => v_image_id
+  ) THEN
+    RAISE insufficient_privilege;
+  END IF;
+
+  WITH deleted AS (
+    DELETE FROM image
+          WHERE image_id = v_image_id
+            AND user_id = v_user_id
+      RETURNING image_id
+  )
+  SELECT count(*)
+    INTO v_deleted
+    FROM deleted
+  ;
+  RETURN json_build_object(
+    'deleted', v_deleted
+  );
+END;
+$$
+LANGUAGE plpgsql
+;
