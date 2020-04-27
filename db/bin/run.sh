e7a011f56a298f7ea8ce581f229347d2298714eb
Liam Tengelis <ltengelis@gmail.com>
2020-04-27 12:27:59 +0700

    remove db from docker-compose

---
+++ db/bin/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+docker run -d \
+       --rm \
+       --net=host \
+       -e POSTGREST_USER=${PGUSER} \
+       -e POSTGREST_PASSWORD=${PGPASSWORD} \
+       -v "/etc/localtime:/etc/localtime:ro" \
+       -v "${DB_DIRNAME}:/var/lib/postgresql/data"
