0e5fa331d37c9408f19c042d90ecf3fa80a6d6d0
Liam Tengelis <ltengelis@gmail.com>
2020-04-27 12:36:49 +0700

    fix db run script

--- db/bin/run.sh
+++ db/bin/run.sh
@@ -7,4 +7,6 @@ docker run -d \
        -e POSTGREST_USER=${PGUSER} \
        -e POSTGREST_PASSWORD=${PGPASSWORD} \
        -v "/etc/localtime:/etc/localtime:ro" \
-       -v "${DB_DIRNAME}:/var/lib/postgresql/data"
+       -v "${DB_DIRNAME}:/var/lib/postgresql/data" \
+       --name galleri_db \
+       postgres:12.2
