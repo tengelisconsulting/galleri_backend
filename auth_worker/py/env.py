84b641c83c9cc4cc32b96d8a375a0954634da6cd
Liam Tengelis <ltengelis@gmail.com>
2020-05-13 15:10:56 +0700

    provide main.py as a generic gist

--- auth_worker/py/env.py
+++ auth_worker/py/env.py
@@ -5,5 +5,6 @@ from typing import NamedTuple
 class _ENV(NamedTuple):
     LISTEN_HOST: str = os.environ["LISTEN_HOST"]
     PORT: int = int(os.environ["PORT"])
+    SERVICE_NAME: str = os.environ["SERVICE_NAME"]
     SESSION_TIMEOUT_S: int = int(os.environ["SESSION_TIMEOUT_S"])
 ENV = _ENV()
